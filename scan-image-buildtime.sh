72ed9dbf4d23d78d61dbf79aa1c87a411adb3ab4
Markus Winkler <winkler.info@icloud.com>
2021-03-23 17:46:57 +0100

    scan-image variants

--- scan-image-buildtime.sh
+++ scan-image-buildtime.sh
@@ -2,43 +2,39 @@
 # ##############################################################################
 # Pulls an image, initiates a scan with Smart Check and creates a PDF report
 # ##############################################################################
-TARGET_IMAGE=${TARGET_IMAGE:-richxsl/rhel7}
-TARGET_IMAGE_TAG=${TARGET_IMAGE_TAG:-latest}
+TARGET_IMAGE=richxsl/rhel7
+TARGET_IMAGE_TAG=latest
 
 SC_NAMESPACE="$(jq -r '.smartcheck_namespace' config.json)"
 SC_USERNAME="$(jq -r '.smartcheck_username' config.json)"
 SC_PASSWORD="$(jq -r '.smartcheck_password' config.json)"
-REG_USERNAME="$(jq -r '.registry_username' config.json)"
-REG_PASSWORD="$(jq -r '.registry_password' config.json)"
-REG_NAMESPACE="$(jq -r '.registry_namespace' config.json)"
-REG_NAME="$(jq -r '.registry_name' config.json)"
-REG_PORT="$(jq -r '.registry_port' config.json)"
+SC_REG_USERNAME="$(jq -r '.smartcheck_reg_username' config.json)"
+SC_REG_PASSWORD="$(jq -r '.smartcheck_reg_password' config.json)"
 
 SC_REGISTRY_PORT="5000"
 SC_API_PORT="443"
 
 SERVICE_HOST=$(kubectl get svc -n ${SC_NAMESPACE} proxy \
             -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
-printf '%s\n' "Smart check is on ${SERVICE_HOST}"
-REGISTRY_HOST=$(kubectl --namespace ${REG_NAMESPACE} get svc ${REG_NAME} \
-                -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
-printf '%s\n' "Cluster registry is on ${REGISTRY_HOST}"
+printf '%s\n' "smart check is on ${SERVICE_HOST}"
 
+SC_REGISTRY="${SERVICE_HOST}:${SC_REGISTRY_PORT}"
 SC_SERVICE="${SERVICE_HOST}:${SC_API_PORT}"
 
 docker pull ${TARGET_IMAGE}:${TARGET_IMAGE_TAG}
-docker tag ${TARGET_IMAGE}:${TARGET_IMAGE_TAG} ${REGISTRY_HOST}:${REG_PORT}/${TARGET_IMAGE}:${TARGET_IMAGE_TAG}
-docker push ${REGISTRY_HOST}:${REG_PORT}/${TARGET_IMAGE}:${TARGET_IMAGE_TAG}
 
 docker run -v /var/run/docker.sock:/var/run/docker.sock --network host \
   deepsecurity/smartcheck-scan-action \
-  --image-name "${REGISTRY_HOST}:${REG_PORT}/${TARGET_IMAGE}:${TARGET_IMAGE_TAG}" \
+  --image-name "${TARGET_IMAGE}:${TARGET_IMAGE_TAG}" \
+  --preregistry-host="$SC_REGISTRY" \
   --smartcheck-host="$SC_SERVICE" \
   --smartcheck-user="$SC_USERNAME" \
   --smartcheck-password="$SC_PASSWORD" \
-  --image-pull-auth=\''{"username":"'${REG_USERNAME}'","password":"'${REG_PASSWORD}'"}'\' \
   --insecure-skip-tls-verify \
-  --insecure-skip-registry-tls-verify
+  --insecure-skip-registry-tls-verify \
+  --preregistry-scan \
+  --preregistry-user "$SC_REG_USERNAME" \
+  --preregistry-password "$SC_REG_PASSWORD"
 
 docker run --network host mawinkler/scan-report:dev -O \
   --name "${TARGET_IMAGE}" \
