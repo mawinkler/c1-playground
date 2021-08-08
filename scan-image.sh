6ce0bfdc4e6f2ed5bf0132828edc9b774e439709
Markus Winkler <winkler.markus@me.com>
2021-08-08 10:55:35 +0200

    macos compatibility

--- scan-image.sh
+++ scan-image.sh
@@ -3,6 +3,8 @@
 # Pulls an image, initiates a scan with Smart Check and creates a PDF report
 # ##############################################################################
 
+OS="$(uname)"
+
 # Test for command line parameter
 if [ -n ${1} ]; then
   TARGET_IMAGE=${1}
@@ -15,40 +17,63 @@ TARGET_IMAGE_TAG=${TARGET_IMAGE_TAG:-latest}
 
 echo "Scanning Image ${TARGET_IMAGE} with tag ${TARGET_IMAGE_TAG}"
 
-SC_NAMESPACE="$(jq -r '.services[] | select(.name=="smartcheck") | .namespace' config.json)"
 SC_USERNAME="$(jq -r '.services[] | select(.name=="smartcheck") | .username' config.json)"
 SC_PASSWORD="$(jq -r '.services[] | select(.name=="smartcheck") | .password' config.json)"
-SC_HOSTNAME="$(jq -r '.services[] | select(.name=="smartcheck") | .hostname' config.json)"
-
+SC_API_PORT="$(jq -r '.services[] | select(.name=="smartcheck") | .proxy_service_port' config.json)"
 REG_USERNAME="$(jq -r '.services[] | select(.name=="playground-registry") | .username' config.json)"
 REG_PASSWORD="$(jq -r '.services[] | select(.name=="playground-registry") | .password' config.json)"
-REG_NAMESPACE="$(jq -r '.services[] | select(.name=="playground-registry") | .namespace' config.json)"
-REG_NAME=playground-registry
-REG_PORT="$(jq -r '.services[] | select(.name=="playground-registry") | .port' config.json)"
+REG_NAME="$(jq -r '.services[] | select(.name=="playground-registry") | .name' config.json)"
 
-SC_REGISTRY_PORT="5000"
-SC_API_PORT="443"
-
-SERVICE_HOST=$(kubectl get svc -n ${SC_NAMESPACE} proxy \
-            -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
-printf '%s\n' "Smart check is on ${SERVICE_HOST}"
-REGISTRY_HOST=$(kubectl --namespace ${REG_NAMESPACE} get svc ${REG_NAME} \
+if [ "${OS}" == 'Linux' ]; then
+  SC_NAMESPACE="$(jq -r '.services[] | select(.name=="smartcheck") | .namespace' config.json)"
+  REG_NAMESPACE="$(jq -r '.services[] | select(.name=="playground-registry") | .namespace' config.json)"
+  REG_PORT="$(jq -r '.services[] | select(.name=="playground-registry") | .port' config.json)"
+  SERVICE_HOST=$(kubectl get svc -n ${SC_NAMESPACE} proxy \
                 -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
-printf '%s\n' "Cluster registry is on ${REGISTRY_HOST}"
+  REGISTRY_HOST=$(kubectl --namespace ${REG_NAMESPACE} get svc ${REG_NAME} \
+                -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+  SC_SERVICE="${SERVICE_HOST}:${SC_API_PORT}"
+fi
 
-SC_SERVICE="${SERVICE_HOST}:${SC_API_PORT}"
+if [ "${OS}" == 'Darwin' ]; then
+  SC_HOSTNAME="$(jq -r '.services[] | select(.name=="smartcheck") | .hostname' config.json)"
+  REGISTRY_HOST="$(jq -r '.services[] | select(.name=="playground-registry") | .hostname' config.json)"
+  REG_PORT=443 # ingress
+  SC_SERVICE="${SC_HOSTNAME}:${SC_API_PORT}"
+fi
+
+
+printf '%s\n' "Smart check is on ${SC_SERVICE}"
+printf '%s\n' "Cluster registry is on ${REGISTRY_HOST}:${REG_PORT}"
 
 echo ${REG_PASSWORD} | docker login ${REGISTRY_HOST}:${REG_PORT} --username ${REG_USERNAME} --password-stdin
 docker pull ${TARGET_IMAGE}:${TARGET_IMAGE_TAG}
 docker tag ${TARGET_IMAGE}:${TARGET_IMAGE_TAG} ${REGISTRY_HOST}:${REG_PORT}/${TARGET_IMAGE}:${TARGET_IMAGE_TAG}
 docker push ${REGISTRY_HOST}:${REG_PORT}/${TARGET_IMAGE}:${TARGET_IMAGE_TAG}
 
-eval docker run --rm --read-only --cap-drop ALL -v /var/run/docker.sock:/var/run/docker.sock --network host \
-  deepsecurity/smartcheck-scan-action \
-  --image-name "${REGISTRY_HOST}:${REG_PORT}/${TARGET_IMAGE}:${TARGET_IMAGE_TAG}" \
-  --smartcheck-host="$SC_SERVICE" \
-  --smartcheck-user="$SC_USERNAME" \
-  --smartcheck-password="$SC_PASSWORD" \
-  --image-pull-auth=\''{"username":"'${REG_USERNAME}'","password":"'${REG_PASSWORD}'"}'\' \
-  --insecure-skip-tls-verify \
-  --insecure-skip-registry-tls-verify &>/dev/null & disown || true
+if [ "${OS}" == 'Darwin' ]; then
+  REGISTRY_HOST=$(kubectl --namespace ${REG_NAMESPACE} get svc ${REG_NAME} \
+                  -o jsonpath='{.spec.clusterIP}')
+  REG_PORT="$(jq -r '.services[] | select(.name=="playground-registry") | .port' config.json)"
+  printf '%s\n' "Cluster registry is on ${REGISTRY_HOST}:${REG_PORT}"
+fi
+
+# eval docker run --rm --read-only --cap-drop ALL -v /var/run/docker.sock:/var/run/docker.sock --network host \
+#   deepsecurity/smartcheck-scan-action \
+#   --image-name "${REGISTRY_HOST}:${REG_PORT}/${TARGET_IMAGE}:${TARGET_IMAGE_TAG}" \
+#   --smartcheck-host="$SC_SERVICE" \
+#   --smartcheck-user="$SC_USERNAME" \
+#   --smartcheck-password="$SC_PASSWORD" \
+#   --image-pull-auth=\''{"username":"'${REG_USERNAME}'","password":"'${REG_PASSWORD}'"}'\' \
+#   --insecure-skip-tls-verify \
+#   --insecure-skip-registry-tls-verify &>/dev/null & disown || true
+
+docker run --rm --read-only --cap-drop ALL -v /var/run/docker.sock:/var/run/docker.sock --network host \
+ deepsecurity/smartcheck-scan-action \
+ --image-name "${REGISTRY_HOST}:${REG_PORT}/${TARGET_IMAGE}:${TARGET_IMAGE_TAG}" \
+ --smartcheck-host="$SC_SERVICE" \
+ --smartcheck-user="$SC_USERNAME" \
+ --smartcheck-password="$SC_PASSWORD" \
+ --image-pull-auth=\''{"username":"'${REG_USERNAME}'","password":"'${REG_PASSWORD}'"}'\' \
+ --insecure-skip-tls-verify \
+ --insecure-skip-registry-tls-verify
