5cc9f14e563495bfd52f6c362478fe9e89adb143
Markus Winkler <winkler.info@icloud.com>
2021-11-18 16:15:09 +0100

    remove

--- dotter.sh
+++
@@ -1,8 +0,0 @@
-n=1
-while [ $n -le 12 ]  
-do
-  echo PAWNED > /tmp/PAWNED-$(date +%H%M%S)
-  chmod +x /tmp/PAWNED*
-  sleep 5
-  n=$(( n+1 ))
-done
--- dotter_cron.sh
+++
@@ -1,7 +0,0 @@
-n=1
-while [ $n -le 12 ]  
-do
-  echo "# PAWNED $(date +%H-%M-%S)" >> /etc/cron.weekly/mdadm
-  sleep 5
-  n=$(( n+1 ))
-done
