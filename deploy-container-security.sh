fdfb30e6d58a8441a42cce961d49835ea6232405
Markus Winkler <winkler.info@icloud.com>
2022-01-14 14:58:34 +0100

    refactoring

--- deploy-container-security.sh
+++ deploy-container-security.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-# source helpers
+# Source helpers
 . ./playground-helpers.sh
 
-# get config
+# Get config
 CLUSTER_NAME="$(jq -r '.cluster_name' config.json)"
 CS_POLICY_NAME="$(jq -r '.services[] | select(.name=="container_security") | .policy_name' config.json)"
 CS_NAMESPACE="$(jq -r '.services[] | select(.name=="container_security") | .namespace' config.json)"
@@ -13,75 +13,113 @@ SC_NAMESPACE="$(jq -r '.services[] | select(.name=="smartcheck") | .namespace' c
 API_KEY="$(jq -r '.services[] | select(.name=="cloudone") | .api_key' config.json)"
 REGION="$(jq -r '.services[] | select(.name=="cloudone") | .region' config.json)"
 
-DEPLOY_RT_YAML=false
-DEPLOY_RT_JSON=false
-if [[ $(kubectl config current-context) =~ gke_.*|aks-.*|.*eksctl.io ]]; then
-  DEPLOY_RT_YAML=true
-  DEPLOY_RT_JSON=true
+DEPLOY_RT=false
+if is_gke || is_aks || is_eks ; then
+  printf '%s' "Deploying with Runtime Security"
+  DEPLOY_RT=true
 fi
 
-# create header
+# Create API header
 API_KEY=${API_KEY} envsubst <templates/cloudone-header.txt >overrides/cloudone-header.txt
 
-function create_namespace {
-
+#######################################
+# Creates Kubernetes namespace
+# Globals:
+#   SC_NAMESPACE
+# Arguments:
+#   None
+# Outputs:
+#   None
+#######################################
+function create_namespace() {
   # create namespace
   printf '%s' "Create container security namespace"
   NAMESPACE=${CS_NAMESPACE} envsubst <templates/namespace.yaml | kubectl apply -f - -o yaml > /dev/null
   printf '%s\n' " 🍼"
 }
 
-function whitelist_namsspaces {
-
+#######################################
+# Whitelists Kubernetes namespace for
+# Container Security
+# Globals:
+#   None
+# Arguments:
+#   None
+# Outputs:
+#   None
+#######################################
+function whitelist_namsspaces() {
   # whitelist some namespace for container security
-  printf '%s\n' "whitelist namespaces"
   kubectl label namespace kube-system --overwrite ignoreAdmissionControl=true
 }
 
-function cluster_policy {
-
+#######################################
+# Checks for an already existing
+# cluster policy. If there is not the
+# desired one it gets created
+# Globals:
+#   REGION
+#   CS_POLICY_NAME
+#   REGISTRY
+# Arguments:
+#   None
+# Outputs:
+#   CS_POLICYID
+#######################################
+function cluster_policy() {
   # query cluster policy
-  printf '%s\n' "query cluster policy id"
   CS_POLICYID=$(
     curl --silent --location --request GET 'https://container.'${REGION}'.cloudone.trendmicro.com/api/policies' \
     --header @overrides/cloudone-header.txt |
-    jq -r --arg CS_POLICY_NAME "${CS_POLICY_NAME}" '.policies[] | select(.name==$CS_POLICY_NAME) | .id'
+      jq -r --arg CS_POLICY_NAME "${CS_POLICY_NAME}" '.policies[] | select(.name==$CS_POLICY_NAME) | .id'
   )
-
   # create policy if not exist
   if [ "${CS_POLICYID}" == "" ]; then
-    printf '%s\n' "getting registry address"
     get_registry
-    printf '%s\n' "registry is on ${REGISTRY}"
-    printf '%s\n' "creating policy ${CS_POLICY_NAME}"
+    printf '%s\n' "Registry is on ${REGISTRY}"
 
     RESULT=$(
       CS_POLICY_NAME=${CS_POLICY_NAME} \
-      REGISTRY=${REGISTRY} \
-      envsubst <templates/container-security-policy.json | \
-        curl --silent --location --request POST 'https://container.'${REGION}'.cloudone.trendmicro.com/api/policies' \
-        --header @overrides/cloudone-header.txt \
-        --data-binary "@-"
+        REGISTRY=${REGISTRY} \
+        envsubst <templates/container-security-policy.json | \
+          curl --silent --location --request POST 'https://container.'${REGION}'.cloudone.trendmicro.com/api/policies' \
+          --header @overrides/cloudone-header.txt \
+          --data-binary "@-"
     )
     CS_POLICYID=$(echo ${RESULT} | jq -r ".id")
-    printf '%s\n' "policy with id ${CS_POLICYID} created"
+    printf '%s\n' "Policy with id ${CS_POLICYID} created"
   else
-    printf '%s\n' "reusing cluster policy with id ${CS_POLICYID}"
+    printf '%s\n' "Reusing cluster policy with id ${CS_POLICYID}"
   fi
 }
 
-function create_cluster_object {
-
+#######################################
+# Creates cluster object in Container
+# Security.
+# Globals:
+#   CLUSTER_NAME
+#   CS_POLICYID
+#   DEPLOY_RT
+#   REGION
+# Arguments:
+#   None
+# Outputs:
+#   API_KEY_ADMISSION_CONTROLLER
+#   CS_CLUSTERID
+#   AP_KEY
+#   AP_SECRET
+#######################################
+function create_cluster_object() {
   # create cluster object
-  printf '%s\n' "create cluster object"
+  printf '%s\n' "Create cluster object"
   RESULT=$(
     CLUSTER_NAME=${CLUSTER_NAME//-/_} \
-    CS_POLICYID=${CS_POLICYID} \
-    DEPLOY_RT_JSON=${DEPLOY_RT_JSON} \
-    envsubst <templates/container-security-cluster-object.json |
-      curl --silent --location --request POST 'https://container.'${REGION}'.cloudone.trendmicro.com/api/clusters' \
-      --header @overrides/cloudone-header.txt \
-      --data-binary "@-"
+      CS_POLICYID=${CS_POLICYID} \
+      DEPLOY_RT=${DEPLOY_RT} \
+      envsubst <templates/container-security-cluster-object.json |
+        curl --silent --location --request POST 'https://container.'${REGION}'.cloudone.trendmicro.com/api/clusters' \
+        --header @overrides/cloudone-header.txt \
+        --data-binary "@-"
   )
   API_KEY_ADMISSION_CONTROLLER=$(echo ${RESULT} | jq -r ".apiKey")
   CS_CLUSTERID=$(echo ${RESULT} | jq -r ".id")
@@ -89,17 +127,28 @@ function create_cluster_object {
   AP_SECRET=$(echo ${RESULT} | jq -r ".runtimeSecret")
 }
 
-function deploy_container_security {
-
+#######################################
+# Deploys Container Security to
+# Kubernetes
+# Globals:
+#   API_KEY_ADMISSION_CONTROLLER
+#   DEPLOY_RT
+#   CS_NAMESPACE
+# Arguments:
+#   None
+# Outputs:
+#   None
+#######################################
+function deploy_container_security() {
   ## deploy container security
-  printf '%s\n' "deploy container security"
+  printf '%s\n' "Deploy container security"
   API_KEY_ADMISSION_CONTROLLER=${API_KEY_ADMISSION_CONTROLLER} \
-  DEPLOY_RT_YAML=${DEPLOY_RT_YAML} \
-    envsubst <templates/overrides-container-security.yaml >overrides/overrides-container-security.yaml
+    DEPLOY_RT=${DEPLOY_RT} \
+    envsubst <templates/container-security-overrides.yaml >overrides/container-security-overrides.yaml
 
   helm upgrade \
     container-security \
-    --values overrides/overrides-container-security.yaml \
+    --values overrides/container-security-overrides.yaml \
     --namespace ${CS_NAMESPACE} \
     --install \
     https://github.com/trendmicro/cloudone-container-security-helm/archive/master.tar.gz
@@ -108,7 +157,7 @@ function deploy_container_security {
   #   echo Running on GKE, AKS or EKS
   #   helm upgrade \
   #     container-security \
-  #     --values overrides/overrides-container-security.yaml \
+  #     --values overrides/container-security-overrides.yaml \
   #     --namespace ${CS_NAMESPACE} \
   #     --install \
   #     https://github.com/trendmicro/cloudone-container-security-helm/archive/master.tar.gz
@@ -116,7 +165,7 @@ function deploy_container_security {
   #   # echo Not running on GKE, AKS or EKS
   #   helm template \
   #     container-security \
-  #     --values overrides/overrides-container-security.yaml \
+  #     --values overrides/container-security-overrides.yaml \
   #     --namespace ${CS_NAMESPACE} \
   #     https://github.com/trendmicro/cloudone-container-security-helm/archive/master.tar.gz | \
   #       sed -e '/\s*\-\sname:\ FALCO_BPF_PROBE/,/\s*value:/d' | \
@@ -124,32 +173,48 @@ function deploy_container_security {
   # fi
 }
 
-function create_scanner {
-
+#######################################
+# Creates a Scanner in Container
+# Security using a locally installed
+# Smart Check
+# Globals:
+#   CLUSTER_NAME
+#   REGION
+# Arguments:
+#   None
+# Outputs:
+#   None
+#######################################
+function create_scanner() {
   # create scanner
-  printf '%s\n' "create scanner object"
+  printf '%s\n' "Create scanner object"
   RESULT=$(
     CLUSTER_NAME=${CLUSTER_NAME//-/_} \
-    envsubst <templates/container-security-scanner.json |
-      curl --silent --location --request POST 'https://container.'${REGION}'.cloudone.trendmicro.com/api/scanners' \
-      --header @overrides/cloudone-header.txt \
-      --data-binary "@-"
+      envsubst <templates/container-security-scanner.json |
+        curl --silent --location --request POST 'https://container.'${REGION}'.cloudone.trendmicro.com/api/scanners' \
+        --header @overrides/cloudone-header.txt \
+        --data-binary "@-"
   )
-
   # bind smartcheck to container security
-  printf '%s\n' "bind smartcheck to container security"
+  printf '%s\n' "Bind smartcheck to container security"
   API_KEY_SCANNER=$(echo ${RESULT} | jq -r ".apiKey") \
   REGION=${REGION} \
-    envsubst <templates/overrides-image-security-bind.yaml >overrides/overrides-image-security-bind.yaml
+    envsubst <templates/container-security-overrides-image-security-bind.yaml >overrides/container-security-overrides-image-security-bind.yaml
 
   helm upgrade \
     smartcheck \
     --reuse-values \
-    --values overrides/overrides-image-security-bind.yaml \
+    --values overrides/container-security-overrides-image-security-bind.yaml \
     --namespace ${SC_NAMESPACE} \
     https://github.com/deep-security/smartcheck-helm/archive/master.tar.gz >/dev/null
 }
 
+#######################################
+# Main:
+# Implements the Container Security
+# deployment. Runtime Security is only
+# activated on GKE, AKS or EKS
+#######################################
 create_namespace
 whitelist_namsspaces
 cluster_policy
--- playground-helpers.sh
+++ playground-helpers.sh
@@ -1,129 +1,210 @@
 #!/bin/bash
 
-function setup_smartcheck {
+#######################################
+# Test for GKE cluster
+# Globals:
+#   None
+# Arguments:
+#   None
+# Returns:
+#   0: if GKE
+#   false: if not GKE
+#######################################
+function is_gke() {
+  if [[ $(kubectl config current-context) =~ gke_.* ]]; then
+    return
+  fi
+  false
+}
 
+#######################################
+# Test for AKS cluster
+# Globals:
+#   None
+# Arguments:
+#   None
+# Returns:
+#   0: if AKS
+#   false: if not AKS
+#######################################
+function is_aks() {
+  if [[ $(kubectl config current-context) =~ .*-aks ]]; then
+    return
+  fi
+  false
+}
+
+#######################################
+# Test for EKS cluster
+# Globals:
+#   None
+# Arguments:
+#   None
+# Returns:
+#   0: if EKS
+#   false: if not EKS
+#######################################
+function is_eks() {
+  if [[ $(kubectl config current-context) =~ .*eksctl.io ]]; then
+    return
+  fi
+  false
+}
+
+#######################################
+# Test for host operating system Linux
+# Globals:
+#   None
+# Arguments:
+#   None
+# Returns:
+#   0: if Linux
+#   false: if not Linux
+#######################################
+function is_linux() {
+  if [ "$(uname)" == 'Linux' ]; then
+    return
+  fi
+  false
+}
+
+#######################################
+# Test for host operating system Darwin
+# Globals:
+#   None
+# Arguments:
+#   None
+# Returns:
+#   0: if Darwin
+#   false: if not Darwin
+#######################################
+function is_darwin() {
+  if [ "$(uname)" == 'Darwin' ]; then
+    return
+  fi
+  false
+}
+
+#######################################
+# Reads basic configuration for Smart
+# Check
+# Globals:
+#   None
+# Arguments:
+#   None
+# Outputs:
+#   SC_USERNAME
+#   SC_PASSWORD
+#   SC_PORT
+#   SC_NAMESPACE
+#######################################
+function setup_smartcheck() {
   SC_USERNAME="$(jq -r '.services[] | select(.name=="smartcheck") | .username' config.json)"
   SC_PASSWORD="$(jq -r '.services[] | select(.name=="smartcheck") | .password' config.json)"
   SC_PORT="$(jq -r '.services[] | select(.name=="smartcheck") | .proxy_service_port' config.json)"
   SC_NAMESPACE="$(jq -r '.services[] | select(.name=="smartcheck") | .namespace' config.json)"
 }
 
-function get_smartcheck {
-
+#######################################
+# Retrieves the url of Smart Check
+# Globals:
+#   OS
+#   SC_NAMESPACE
+# Arguments:
+#   None
+# Outputs:
+#   SC_HOST
+#######################################
+function get_smartcheck() {
   setup_smartcheck
-  
   # gke
-  if [[ $(kubectl config current-context) =~ gke_.* ]]; then
-
-    printf '%s\n' "running on gke"
+  if is_gke ; then
+    printf '%s\n' "Running on gke"
     SC_HOST=$(kubectl get svc -n ${SC_NAMESPACE} proxy \
               -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
-
   # aks
-  elif [[ $(kubectl config current-context) =~ .*-aks ]]; then
-
-    printf '%s\n' "running on aks"
+  elif is_aks ; then
+    printf '%s\n' "Running on aks"
     SC_HOST=$(kubectl get svc -n ${SC_NAMESPACE} proxy \
               -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
-
   # eks
-  elif [[ $(kubectl config current-context) =~ .*eksctl.io ]]; then
-
-    printf '%s\n' "running on eks"
+  elif is_eks ; then
+    printf '%s\n' "Running on eks"
     printf '%s\n' "NOT YET IMPLEMENTED"
     exit 0
-
   # local
   else
-
-    printf '%s\n' "running on local playground"
-    if [ "${OS}" == 'Linux' ]; then
+    printf '%s\n' "Running on local playground"
+    if is_linux ; then
       SC_HOST=$(kubectl get svc -n ${SC_NAMESPACE} proxy \
                     -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
     fi
-
-    if [ "${OS}" == 'Darwin' ]; then
+    if is_darwin ; then
       SC_HOST="$(jq -r '.services[] | select(.name=="smartcheck") | .hostname' config.json)"
     fi
-
   fi
 }
 
-function get_registry {
-
+#######################################
+# Retrieves the url of the registry
+# Globals:
+#   GCP_HOSTNAME
+#   GCP_PROJECTID
+#   PLAYGROUND_NAME
+#   REGISTRY_NAME
+#   REG_NAMESPACE
+#   REG_NAME
+# Arguments:
+#   None
+# Outputs:
+#   REGISTRY
+#######################################
+function get_registry() {
   # gke
-  if [[ $(kubectl config current-context) =~ gke_.* ]]; then
-
-    printf '%s\n' "running on gke"
+  if is_gke ; then
+    printf '%s\n' "Running on gke"
     GCP_HOSTNAME="gcr.io"
     GCP_PROJECTID=$(gcloud config list --format 'value(core.project)' 2>/dev/null)
     REGISTRY=${GCP_HOSTNAME}/${GCP_PROJECTID}
-
   # aks
-  elif [[ $(kubectl config current-context) =~ .*-aks ]]; then
-
-    printf '%s\n' "running on aks"
+  elif is_aks ; then
+    printf '%s\n' "Running on aks"
     PLAYGROUND_NAME="$(jq -r '.cluster_name' config.json)"
-
     if [[ $(az group list | jq -r --arg PLAYGROUND_NAME ${PLAYGROUND_NAME} '.[] | select(.name==$PLAYGROUND_NAME) | .name') == "" ]]; then
-
       printf '%s\n' "creating resource group ${PLAYGROUND_NAME}"
       az group create --name ${PLAYGROUND_NAME} --location westeurope
-
     else
-
       printf '%s\n' "using resource group ${PLAYGROUND_NAME}"
-
     fi
-
     REGISTRY_NAME=$(az acr list --resource-group ${PLAYGROUND_NAME} | jq -r --arg PLAYGROUND_NAME ${PLAYGROUND_NAME//-/} '.[] | select(.name | startswith($PLAYGROUND_NAME)) | .name')
-
     if [[ ${REGISTRY_NAME} == "" ]]; then
-
       REGISTRY_NAME=${PLAYGROUND_NAME//-/}$(openssl rand -hex 4)
       printf '%s\n' "creating container registry ${REGISTRY_NAME}"
       az acr create --resource-group ${PLAYGROUND_NAME} --name ${REGISTRY_NAME} --sku Basic
-
     else
-
       printf '%s\n' "using container registry ${REGISTRY_NAME}"
-
     fi
-
     REGISTRY=$(az acr show --resource-group ${PLAYGROUND_NAME} --name ${REGISTRY_NAME} -o json | jq -r '.loginServer')
-
   # eks
-  elif [[ $(kubectl config current-context) =~ .*eksctl.io ]]; then
-
-    printf '%s\n' "running on eks"
+  elif is_eks ; then
+    printf '%s\n' "Running on eks"
     printf '%s\n' "NOT YET IMPLEMENTED"
     exit 0
-
   # local
   else
-
-    printf '%s\n' "running on local playground"
+    printf '%s\n' "Running on local playground"
     REG_NAME="$(jq -r '.services[] | select(.name=="playground-registry") | .name' config.json)"
     REG_NAMESPACE="$(jq -r '.services[] | select(.name=="playground-registry") | .namespace' config.json)"
-    OS="$(uname)"
-
-    if [ "${OS}" == 'Linux' ]; then
-
+    if is_linux ; then
       REG_HOST=$(kubectl --namespace ${REG_NAMESPACE} get svc ${REG_NAME} \
                     -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
       REG_PORT="$(jq -r '.services[] | select(.name=="playground-registry") | .port' config.json)"
-
     fi
-
-    if [ "${OS}" == 'Darwin' ]; then
-
+    if is_darwin ; then
       REG_HOST=$(kubectl --namespace ${REG_NAMESPACE} get svc ${REG_NAME} \
                       -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
       REG_PORT="$(jq -r '.services[] | select(.name=="playground-registry") | .port' config.json)"
-
     fi
-
     REGISTRY="${REG_HOST}:${REG_PORT}"
-
   fi
 }
