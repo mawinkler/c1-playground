58b98ceef5dd44a4dff9923f3013c96c9fecd5c8
Markus Winkler <winkler.info@icloud.com>
2022-01-18 13:24:17 +0100

    falco

--- deploy-falco.sh
+++ deploy-falco.sh
@@ -2,33 +2,43 @@
 
 set -e
 
+# Source helpers
+. ./playground-helpers.sh
+
+# Get config
 NAMESPACE="$(jq -r '.services[] | select(.name=="falco") | .namespace' config.json)"
 HOSTNAME="$(jq -r '.services[] | select(.name=="falco") | .hostname' config.json)"
 SERVICE_NAME="$(jq -r '.services[] | select(.name=="falco") | .proxy_service_name' config.json)"
 LISTEN_PORT="$(jq -r '.services[] | select(.name=="falco") | .proxy_listen_port' config.json)"
-OS="$(uname)"
 
-if [[ $(kubectl config current-context) =~ gke_.*|aks-.*|.*eksctl.io ]]; then
-  echo Running on GKE, AKS or EKS
-fi
-
-function create_namespace {
+#######################################
+# Creates Kubernetes namespace
+# Globals:
+#   NAMESPACE
+# Arguments:
+#   None
+# Outputs:
+#   None
+#######################################
+function create_namespace() {
+  # create namespace
   printf '%s' "Create falco namespace"
-
-  # create service
-  cat <<EOF | kubectl apply -f - -o yaml
-apiVersion: v1
-kind: Namespace
-metadata:
-  name: ${NAMESPACE}
-EOF
+  NAMESPACE=${NAMESPACE} envsubst <templates/namespace.yaml | kubectl apply -f - -o yaml > /dev/null
   printf '%s\n' " 🍼"
 }
 
-function whitelist_namsspace {
-  printf '%s\n' "Whitelist namespace"
-
-  # whitelist namespace for falco
+#######################################
+# Whitelists Kubernetes namespace for
+# Falco
+# Globals:
+#   None
+# Arguments:
+#   None
+# Outputs:
+#   None
+#######################################
+function whitelist_namsspace() {
+  # whitelist some namespace for container security
   kubectl label namespace ${NAMESPACE} --overwrite ignoreAdmissionControl=true
 }
 
@@ -45,8 +55,16 @@ function whitelist_namsspace {
 # cmake ..
 # sudo make install_driver
 ###
-
-function deploy_falco {
+#######################################
+# Deploys Falco to Kubernetes
+# Globals:
+#   NAMESPACE
+# Arguments:
+#   None
+# Outputs:
+#   None
+#######################################
+function deploy_falco() {
   ## deploy falco
   printf '%s\n' "deploy falco"
 
@@ -54,55 +72,35 @@ function deploy_falco {
   helm repo update
 
   mkdir -p overrides
-  cat <<EOF > overrides/overrides-falco.yaml
-auditLog:
-  enabled: true
-falco:
-  jsonOutput: true
-  jsonIncludeOutputProperty: true
-  grpc:
-    enabled: true
-  grpcOutput:
-    enabled: true
-falcosidekick:
-  enabled: true
-  webui:
-    enabled: true
-    service:
-      type: LoadBalancer
-EOF
+  envsubst <templates/falco-overrides.yaml >overrides/falco-overrides.yaml
 
   # If running on GKE or AKS we switch to eBPF
-  if [[ $(kubectl config current-context) =~ gke_.*|aks-.*|.*eksctl.io|kind.* ]]; then
-    cat <<EOF >> overrides/overrides-falco.yaml
-ebpf:
-  enabled: true
-EOF
+  if is_gke || is_aks || is_eks || is_kind ; then
+    echo "ebpf:" >> overrides/falco-overrides.yaml
+    echo "  enabled: true" >> overrides/falco-overrides.yaml
   fi
 
-  cat <<EOF > overrides/custom-rules.yaml
-customRules:
-EOF
+  echo "customRules:" > overrides/falco-custom-rules.yaml
 
-  # If there is a file called `falco/playground_rules_dev.yaml`, we append it to the custom-rules.yaml
+  # If there is a file called `falco/playground_rules_dev.yaml`, we append it to the falco-custom-rules.yaml
   # and skip the playground and additional rule files
   if [ -f "falco/playground_rules_dev.yaml" ]; then
     printf '%s\n' "Playground Dev rules file found"
-    echo "  a_playground_rules_dev.yaml: |-" >> overrides/custom-rules.yaml
-    cat falco/playground_rules_dev.yaml | sed  -e 's/^/    /' >> overrides/custom-rules.yaml
+    echo "  a_playground_rules_dev.yaml: |-" >> overrides/falco-custom-rules.yaml
+    cat falco/playground_rules_dev.yaml | sed  -e 's/^/    /' >> overrides/falco-custom-rules.yaml
   else    
-    # If there is a file called `falco/playground_rules.yaml`, we append it to the custom-rules.yaml
+    # If there is a file called `falco/playground_rules.yaml`, we append it to the falco-custom-rules.yaml
     if [ -f "falco/playground_rules.yaml" ]; then
       printf '%s\n' "Playground rules file found"
-      echo "  a_playground_rules.yaml: |-" >> overrides/custom-rules.yaml
-      cat falco/playground_rules.yaml | sed  -e 's/^/    /' >> overrides/custom-rules.yaml
+      echo "  a_playground_rules.yaml: |-" >> overrides/falco-custom-rules.yaml
+      cat falco/playground_rules.yaml | sed  -e 's/^/    /' >> overrides/falco-custom-rules.yaml
     fi
 
-    # If there is a file called `falco/additional_rules.yaml`, we append it to the custom-rules.yaml
+    # If there is a file called `falco/additional_rules.yaml`, we append it to the falco-custom-rules.yaml
     if [ -f "falco/additional_rules.yaml" ]; then
       printf '%s\n' "Additional rules file found"
-      echo "  z_additional_rules.yaml: |-" >> overrides/custom-rules.yaml
-      cat falco/additional_rules.yaml | sed  -e 's/^/    /' >> overrides/custom-rules.yaml
+      echo "  z_additional_rules.yaml: |-" >> overrides/falco-custom-rules.yaml
+      cat falco/additional_rules.yaml | sed  -e 's/^/    /' >> overrides/falco-custom-rules.yaml
     fi
   fi
 
@@ -112,8 +110,8 @@ EOF
   helm -n ${NAMESPACE} upgrade \
     falco \
     --install \
-    --values=overrides/overrides-falco.yaml \
-    -f overrides/custom-rules.yaml \
+    --values=overrides/falco-overrides.yaml \
+    -f overrides/falco-custom-rules.yaml \
     falcosecurity/falco
 
   helm -n ${NAMESPACE} upgrade \
@@ -122,52 +120,32 @@ EOF
     falcosecurity/falco-exporter
 
   # Create NodePort Service to enable K8s Audit
-  cat <<EOF | kubectl -n ${NAMESPACE} apply -f -
-kind: Service
-apiVersion: v1
-metadata:
-  name: falco-np
-spec:
-  selector:
-    app: falco
-  ports:
-  - protocol: TCP
-    port: 8765
-    nodePort: 32765
-  type: NodePort
-EOF
+  envsubst <templates/falco-nodeport-service.yaml | kubectl -n ${NAMESPACE} apply -f -
 }
 
-function create_ingress {
-    # create ingress for prometheus and grafana
-
-  printf '%s\n' "Create prometheus and grafana ingress"
-  cat <<EOF | kubectl apply -f - -o yaml
-apiVersion: networking.k8s.io/v1
-kind: Ingress
-metadata:
-  annotations:
-    kubernetes.io/ingress.class: nginx
-    nginx.ingress.kubernetes.io/backend-protocol: HTTP
-  name: ${SERVICE_NAME}
-  namespace: ${NAMESPACE}
-spec:
-  rules:
-    - host: ${HOSTNAME}
-      http:
-        paths:
-        - path: /
-          pathType: Prefix
-          backend:
-            service:
-              name: ${SERVICE_NAME}
-              port:
-                number: ${LISTEN_PORT}
-EOF
-  printf '%s\n' "Prometheus and grafana ingress created 🍻"
+#######################################
+# Creates Kubernetes ingress
+# Globals:
+#   SERVICE_NAME
+#   NAMESPACE
+#   HOSTNAME
+#   LISTEN_PORT
+# Arguments:
+#   None
+# Outputs:
+#   None
+#######################################
+function create_ingress() {
+  printf '%s\n' "Create falco ingress"
+  SERVICE_NAME=${SERVICE_NAME} \
+    NAMESPACE=${NAMESPACE} \
+    HOSTNAME=${HOSTNAME} \
+    LISTEN_PORT=${LISTEN_PORT} \
+    envsubst <templates/falco-ingress.yaml | kubectl apply -f - -o yaml
+  printf '%s\n' "Falco ingress created 🍻"
 }
 
-if [ "${OS}" == 'Darwin' ]; then
+if is_darwin ; then
   echo "*** Falco currently not supported on MacOS ***"
   exit 0
 fi
@@ -176,14 +154,14 @@ create_namespace
 whitelist_namsspace
 deploy_falco
 
-if [ "${OS}" == 'Linux' ]; then
-  # test if we're using a managed kubernetes cluster on GCP, Azure (or AWS)
-  if [[ ! $(kubectl config current-context) =~ gke_.*|aks-.*|.*eksctl.io ]]; then
+if is_linux ; then
+  # test if we're using a kind cluster and need a proxy
+  if is_kind ; then
     ./deploy-proxy.sh falco
     HOST_IP=$(hostname -I | awk '{print $1}')
-    echo "Falco UI on: http://${HOST_IP}:${LISTEN_PORT}/ui/#/" >> services
+    echo "Falco UI on: http://${HOST_IP}:${LISTEN_PORT}/ui/#/" | tee -a services
   fi
 fi
-if [ "${OS}" == 'Darwin' ]; then
+if is_darwin ; then
   create_ingress
 fi
