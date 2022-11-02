12527b06a53a0a2a0bf51346e928f4f3102454a1
mawinkler <winkler.info@icloud.com>
2022-11-02 17:07:49 +0000

    update mode

--- tools/aws2-install.sh
+++ tools/aws2-install.sh
@@ -9,6 +9,6 @@
 
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "/tmp/awscliv2.zip"
 unzip /tmp/awscliv2.zip -d /tmp
-sudo /tmp/aws/install
+sudo /tmp/aws/install --update
 rm -Rf /tmp/aws /tmp/awscliv2.zip
 aws --version
