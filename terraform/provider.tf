a4c1909316244e22140e97068ae9f929b19ee607
mawinkler <winkler.info@icloud.com>
2023-02-06 10:19:17 +0000

    ec2

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,4 @@
 provider "aws" {
     region = "${var.AWS_REGION}"
+    profile = "default"
 }
--- terraform/vars.tf
+++ terraform/vars.tf
@@ -13,12 +13,12 @@ variable "PUBLIC_KEY_PATH" {
 variable "EC2_USER" {
   default = "ubuntu"
 }
-variable "AMI" {
-  type = map
+# variable "AMI" {
+#   type = map
 
-  default = {
-    "eu-west-2" = "ami-03dea29b0216a1e03"
-    "us-east-1" = "ami-0c2a1acae6667e438"
-    "eu-central-1" = "ami-03e08697c325f02ab"
-  }
-}
+#   default = {
+#     "eu-west-2" = "ami-03dea29b0216a1e03"
+#     "us-east-1" = "ami-0c2a1acae6667e438"
+#     "eu-central-1" = "ami-03e08697c325f02ab"
+#   }
+# }
